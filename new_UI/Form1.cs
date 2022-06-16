836e62e7bd42c3358804b0c928e1c3ae26f4d2b8
Nattapon150745 <107375395+Nattapon150745@users.noreply.github.com>
2022-06-16 17:59:41 +0700

    Add files via upload

---
+++ new_UI/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mini_project0._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static string I_ID = "";
+        public static string A_adderss = "";
+        public static string N_name = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            I_ID = textBox1.Text;
+            A_adderss = textBox2.Text;
+            N_name = textBox3.Text;
+            textBox4.Text = DateTime.Now.ToString();
+            MessageBox.Show(I_ID + " " + A_adderss + " " + N_name + " " + textBox4.Text);
+
+            new Form2().Show();
+            this.Hide();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
