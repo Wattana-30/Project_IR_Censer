adecb8dffad5165fd0abf4ef7538598b36242b3b
tomod112 <107185203+tomod112@users.noreply.github.com>
2022-06-16 14:20:08 +0700

    1

--- API+DB/API.py
+++ API+DB/API.py
@@ -2,9 +2,9 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
   host="localhost",
-  user="logtast",
-  password="logtast",
-  database="logtast"
+  user="project_ir_censer,
+  password="123456",
+  database="project_ir_censer"
 )
 
 print(mydb)
